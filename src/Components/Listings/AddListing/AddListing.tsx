74be19c81bf3f0df4822c7203eb19f12c807d5cc
Ivan Pavić <TristanRedrose@hotmail.com>
2022-09-20 10:43:12 +0200

    change addNewList form style

--- src/Components/Listings/AddListing/AddListing.tsx
+++ src/Components/Listings/AddListing/AddListing.tsx
@@ -22,26 +22,44 @@ const AddListing: React.FC = observer(() => {
             <div className="listing-title">
                 <h2 className="lobster-text">Add Listing</h2>
             </div>
-            <form className="form-container" onSubmit={(e) => {e.preventDefault(); addNewListing()}}>
-                <div className="form-select-container">
-                    <select className="form-select" required name="company" defaultValue="" title="company-filter" onChange={(e) => setNewListingValue(e)}>
-                        <option value="" disabled>Company</option>
-                        {companyList.map(item => {
-                            return <option key={item.id} value={item.company}>{item.company}</option>
-                        })}
-                    </select>
-                    <select className="form-select" required name="engine" title="company-filter" defaultValue="" onChange={(e) => setNewListingValue(e)}>
-                        <option value="" disabled>Engine</option>
-                        <option value="petrol">Petrol</option>
-                        <option value="diesel">Diesel</option>
-                    </select>
-                </div>
-                <input name="type" className="form-input" type="text" placeholder="Type" onChange={(e) => setNewListingValue(e)} />
-                <input name="horsepower" className="form-input" type="number" min="80" max="150" placeholder="Horsepower" onChange={(e) => setNewListingValue(e)}/>
-                <input name="price" className="form-input" type="number" min="0" placeholder="Price" onChange={(e) => setNewListingValue(e)}/>
-                <input name="image" className="form-input" type="text" placeholder="Image-url" onChange={(e) => setNewListingValue(e)}/>
-                <input className="button-div" type="submit"/>
-            </form>
+            <div className="form-container">
+                <form className="add-listing-form" onSubmit={(e) => {e.preventDefault(); addNewListing()}}>
+                    <div className="form-select-container">
+                        <select className="form-select" required name="company" defaultValue="" title="company-filter" onChange={(e) => setNewListingValue(e)}>
+                            <option value="" disabled>Company</option>
+                            {companyList.map(item => {
+                                return <option key={item.id} value={item.company}>{item.company}</option>
+                            })}
+                        </select>
+                        <select className="form-select" required name="engine" title="company-filter" defaultValue="" onChange={(e) => setNewListingValue(e)}>
+                            <option value="" disabled>Engine</option>
+                            <option value="petrol">Petrol</option>
+                            <option value="diesel">Diesel</option>
+                        </select>
+                    </div>
+                    <div className="input-div">
+                        <div className="form-input-container">
+                            <label htmlFor="type">Type:</label>
+                            <input name="type" className="form-input" type="text" placeholder="Type" onChange={(e) => setNewListingValue(e)} />
+                        </div>
+                        <div className="form-input-container">
+                            <label htmlFor="type">Horsepower:</label>
+                            <input name="horsepower" className="form-input" type="number" min="80" max="150" placeholder="Horsepower" onChange={(e) => setNewListingValue(e)}/>
+                        </div>
+                    </div>
+                    <div className="input-div">
+                        <div className="form-input-container">
+                            <label htmlFor="type">Price:</label>
+                            <input name="price" className="form-input" type="number" min="0" placeholder="Price" onChange={(e) => setNewListingValue(e)}/>
+                        </div>
+                        <div className="form-input-container">
+                            <label htmlFor="type">Image:</label>
+                            <input name="image" className="form-input" type="text" placeholder="Image-url" onChange={(e) => setNewListingValue(e)}/>
+                        </div>
+                    </div>
+                    <input className="button-div" type="submit"/>
+                </form>
+            </div>
             {message && <h4 className="form-message">{message}</h4>}
         </div>
     )
